f87961b4042ae2a627da5ab5f1c3a0cb321e4d78
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-05-08 20:20:31 -0300

    Update counter_bloc.dart

--- lib/blocs/counter_bloc.dart
+++ lib/blocs/counter_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-sealed class CounterEvent {}
+abstract class CounterEvent {}
 
 final class CounterIncrementEvent extends CounterEvent {}
 final class CounterDecrementEvent extends CounterEvent {}
@@ -12,12 +12,10 @@ class CounterBloc extends Bloc<CounterEvent, int> {
   }
 
   Future<void> _onIncrement(CounterIncrementEvent event, Emitter<int> emit) async {
-    // handle incoming `CounterIncrementEvent` event
     emit(state + 1);
   }
 
   Future<void> _onDecrement(CounterDecrementEvent event, Emitter<int> emit) async {
-    // handle incoming `CounterIncrementEvent` event
     emit(state - 1);
   }
 
